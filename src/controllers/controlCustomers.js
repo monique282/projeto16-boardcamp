cb302669b3c5df0a25c8f5f0a9df830964f70b57
monique <moniquejp16@gmail.com>
2023-07-27 17:57:56 -0300

    feat: adicionando o get de clietes por id

--- src/controllers/controlCustomers.js
+++ src/controllers/controlCustomers.js
@@ -3,12 +3,28 @@
 
 import { db } from "../database/db.js";
 
-// esse arquivo aqui é enviado por um get para pegar a lista de jogos
+// essa função aqui é enviado por um get para pegar a lista de jogos
 export async function customersGet(req, res) {
     try {
 
         const customersRequest = await db.query(`SELECT * FROM customers;`)
         res.send(customersRequest);
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+
+}
+
+// essa função aqui é enviado por um get para pegar a lista de jogos pelo id
+
+export async function customersGetId(req, res) {
+    const { id } = req.params;
+
+    try {
+
+        const CustomersRequestByid = await db.query(
+            `SELECT * FROM customers WHERE id=$1;`, [id])
+        res.send(CustomersRequestByid);
     } catch (err) {
         res.status(500).send(err.message)
     }
--- src/routes/customersRoutes.js
+++ src/routes/customersRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { customersGet } from "../controllers/controlCustomers.js";
+import { customersGet, customersGetId } from "../controllers/controlCustomers.js";
 
 
 
 const customerRouter = Router();
 
 customerRouter.get("/customers", customersGet);
+customerRouter.get("/customers:id", customersGetId);
 
 
 export default customerRouter;
