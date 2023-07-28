125ebd2e94a8f86ea1bb5fad2e31bc38e73a8b0d
monique <moniquejp16@gmail.com>
2023-07-28 14:27:33 -0300

    fix: corrigindo o  joi de clientes

--- src/controllers/controlCustomers.js
+++ src/controllers/controlCustomers.js
@@ -28,5 +28,37 @@ export async function customersGetId(req, res) {
     } catch (err) {
         res.status(500).send(err.message)
     }
+}
 
+// essa função aqui é enviado por um post para crir a lista de cliente
+
+export async function customersPost(req, res) {
+    // pegar os dados que a pessoa colocou na tela de cadastro de cliente
+    const { name, phone, cpf, birthday } = req.body;
+
+    try {
+        // pegando toda a lista de jogos
+        const listGames = await db.query(`
+        SELECT * FROM games
+        `)
+        // verificar se o nome ja foi cadastrado
+        let nameExists = false;
+        listGames.rows.forEach(game => {
+            if (game.name === name) {
+                nameExists = true;
+                return;
+            }
+        });
+        if (nameExists) {
+            return res.sendStatus(409);
+        }
+        const insertGame = await db.query(`
+            INSERT INTO games (name, image, "stockTotal", "pricePerDay") VAlUES ($1, $2, $3, $4);
+            ` , [name, image, stockTotal, pricePerDay]);
+            return res.sendStatus(201);
+
+    } catch (err) {
+        res.status(500).send(err.message)
+
+    }
 }
--- src/schemas/customersSchema.js
+++ src/schemas/customersSchema.js
@@ -3,7 +3,7 @@ import joi from "joi";
 export const gameTable = joi.object({
 
     name: joi.string().max(64).required(),
-    phone: joi.number().min(10).required(),
-    cpf: joi.number().min(11).required(),
+    phone: joi.number().min(10).max(11).pattern(/^\d+$/).required(),
+    cpf: joi.number().length(11).pattern(/^\d+$/).required(),
     birthday: joi.string().required(),
 })
