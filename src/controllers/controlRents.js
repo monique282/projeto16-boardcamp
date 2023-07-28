610e7a973aa9045ea0ddf6d8cacfd2af5bfc9f10
monique <moniquejp16@gmail.com>
2023-07-28 15:53:03 -0300

    fix: corrigindo joi de costumers: birthday

--- src/controllers/controlRents.js
+++ src/controllers/controlRents.js
@@ -8,8 +8,41 @@ export async function rentsGet(req, res) {
     try {
         const rentsRequest = await db.query(`SELECT * FROM rentals;`)
         res.send(rentsRequest);
+
     } catch (err) {
         res.status(500).send(err.message)
     }
 
+}
+
+export async function rentsPost(req, res) {
+    // pegar os dados que a pessoa colocou na tela de alugueis
+    const { customerId, gameId, daysRented} = req.body;
+    
+    try {
+        // pegando toda a lista de clientes
+        const listCustomers = await db.query(`
+        SELECT * FROM rents;
+        `)
+        // verificar se o cpf ja foi cadastrado
+        let cpfExists = false;
+        listCustomers.rows.forEach(customer => {
+            if (customer.cpf === cpf) {
+                cpfExists = true;
+                return;
+            }
+        });
+        if (cpfExists) {
+            return res.sendStatus(409);
+        }
+        // se tivertudo certo enviar para o Api
+        const insertCustomers = await db.query(`
+            INSERT INTO customers (name, phone, cpf, birthday) VAlUES ($1, $2, $3, $4);
+            ` , [name, phone, cpf, birthday]);
+        return res.sendStatus(201);
+
+    } catch (err) {
+        res.status(500).send(err.message)
+
+    }
 }
--- src/schemas/customersSchema.js
+++ src/schemas/customersSchema.js
@@ -5,5 +5,5 @@ export const customersTable = joi.object({
     name: joi.string().min(1).required(),
     phone: joi.string().pattern(new RegExp('^[0-9]{10,11}$')),
     cpf: joi.string().pattern(new RegExp('^[0-9]{11,11}$')),
-    birthday: joi.string().required(),
+    birthday: joi.date().required(),
 })
