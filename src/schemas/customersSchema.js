2e890800b3bc32bc1b9c5e5515b8bcf71ecfed89
monique <moniquejp16@gmail.com>
2023-07-28 14:03:46 -0300

    feat: adicionando o joi dos clientes

--- src/schemas/customersSchema.js
+++ src/schemas/customersSchema.js
@@ -5,6 +5,5 @@ export const gameTable = joi.object({
     name: joi.string().max(64).required(),
     phone: joi.number().min(10).required(),
     cpf: joi.number().min(11).required(),
-    // preço por dia
     birthday: joi.string().required(),
 })
