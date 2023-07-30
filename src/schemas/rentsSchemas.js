efe08f909ff70ee8c1bfff5a3325da2e49eff2e2
monique <moniquejp16@gmail.com>
2023-07-30 15:19:55 -0300

    refector: ageitando o codigo rentsSchemas

--- src/schemas/rentsSchemas.js
+++ src/schemas/rentsSchemas.js
@@ -4,10 +4,10 @@ export const rentsTable = joi.object({
 
     // id do ususario
     customerId: joi.string().min(1).required(),
+
     // id do jogo
     gameId: joi.string().min(1).required(),
+
     // quantidade de dias de aluguel do jogo
     daysRented: joi.number().min(1).required(),
-   
-
-})
+});
