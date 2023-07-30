89d87fa5794ccc649356e9c24d7e5f649fb2bc6b
monique <moniquejp16@gmail.com>
2023-07-30 15:08:51 -0300

    refector: ageitando o codigo controlGame

--- src/controllers/controlGame.js
+++ src/controllers/controlGame.js
@@ -4,46 +4,49 @@
 import { db } from "../database/db.js";
 
 // essa função aqui é enviado por um get para pegar a lista de jogos
+
 export async function gameGet(req, res) {
     try {
-        const result = await db.query(`SELECT * FROM games;`)
-        const gameRequest = result.rows
+        const result = await db.query(`SELECT * FROM games;`);
+        const gameRequest = result.rows;
         res.send(gameRequest);
     } catch (err) {
-        res.status(500).send(err.message)
-    }
-
-}
+        res.status(500).send(err.message);
+    };
+};
 
 // essa função aqui é enviado por um post para enviar uma lista de jogos
 
 export async function gamePost(req, res) {
+
     // pegar os dados que a pessoa colocou na tela de cadastro
     const { name, image, stockTotal, pricePerDay } = req.body;
+
     try {
         // pegando toda a lista de jogos
         const listGames = await db.query(`
         SELECT * FROM games;
-        `)
+        `);
+
         // verificar se o nome ja foi cadastrado
         let nameExists = false;
         listGames.rows.forEach(game => {
             if (game.name === name) {
                 nameExists = true;
                 return;
-            }
+            };
         });
+
         if (nameExists) {
             return res.sendStatus(409);
-        }
+        };
+
         const insertGame = await db.query(`
             INSERT INTO games (name, image, "stockTotal", "pricePerDay") VAlUES ($1, $2, $3, $4);
             ` , [name, image, stockTotal, pricePerDay]);
             return res.sendStatus(201);
 
     } catch (err) {
-        res.status(500).send(err.message)
-
-    }
-
+        res.status(500).send(err.message);
+    };
 };
