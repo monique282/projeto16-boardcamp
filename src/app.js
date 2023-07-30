e5c94c7d83e8f13bf8602dd1d78642fac0f47fb9
monique <moniquejp16@gmail.com>
2023-07-30 15:43:02 -0300

    refector: conficuração para o deploy app novamente

--- src/app.js
+++ src/app.js
@@ -3,18 +3,15 @@ import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes/indexRoutes.js";
 
-// configuração da API
+//configuração da API
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(router);
+app.use(router)
 
-const port = process.env.PORT || 5000;
-
-// cerve pra deixar a aplicação ligada na porta escolhida
+const port = process.env.PORT || 5000
 app.listen(port, () => {
-	console.log(`Servidor rodando na porta ${port}`)
+    console.log(`Servidor rodando na porta ${port}`)
 });
-
