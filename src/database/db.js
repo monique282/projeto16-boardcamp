0f732e0e146d6e867bc150630b6cbcd5fb53256f
monique <moniquejp16@gmail.com>
2023-07-30 15:43:41 -0300

    refector: conficuração para o deploy db

--- src/database/db.js
+++ src/database/db.js
@@ -6,8 +6,10 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const connection = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  });
+const configDatabase = {
+  connectionString: process.env.DATABASE_URL,
+};
 
-export const db = connection;
+if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
+
+export const db = new Pool(configDatabase);
