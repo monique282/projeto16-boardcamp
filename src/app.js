31a658674b8c3ad950a87353e251b28410a99680
monique <moniquejp16@gmail.com>
2023-07-30 15:21:28 -0300

    refector: ageitando o codigo app

--- src/app.js
+++ src/app.js
@@ -11,8 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(router)
 
-
-
 const port = process.env.PORT || 5000;
+
 // cerve pra deixar a aplicação ligada na porta escolhida
 app.listen(port, () => console.log(`Servidor rodando na porta: ${port}`));
