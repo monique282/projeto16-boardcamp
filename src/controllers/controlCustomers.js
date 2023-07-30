337a70debba2723e5c5caa25b70c368cfcaac7ac
monique <moniquejp16@gmail.com>
2023-07-30 14:59:31 -0300

    refector: ageitando o codigo controlCustomers

--- src/controllers/controlCustomers.js
+++ src/controllers/controlCustomers.js
@@ -6,9 +6,11 @@ import { parseISO, format } from 'date-fns';
 
 // essa função aqui é enviado por um get para pegar a lista de clientes
 export async function customersGet(req, res) {
+
     try {
 
         const result = await db.query(`SELECT * FROM customers;`);
+
         // tratando a data para vim no formato correto
         const updatedData = result.rows.map(date => {
             const dateCorret = new Date(date.birthday);
@@ -16,15 +18,15 @@ export async function customersGet(req, res) {
             return {
                 ...date,
                 birthday: formatDate
-            }
+            };
         });
 
         res.send(updatedData);
-    } catch (err) {
-        res.status(500).send(err.message)
-    }
 
-}
+    } catch (err) {
+        res.status(500).send(err.message);
+    };
+};
 
 // essa função aqui é enviado por um get para pegar a lista de clientes pelo id
 
@@ -38,8 +40,9 @@ export async function customersGetId(req, res) {
 
         // verificando se o id existe
         if (result.rows.length === 0) {
-            return res.sendStatus(404)
-        }
+            return res.sendStatus(404);
+        };
+
         // tratando a data para vim no formato correto
         const updatedData = result.rows.map(date => {
             const dateCorret = new Date(date.birthday);
@@ -47,37 +50,43 @@ export async function customersGetId(req, res) {
             return {
                 ...date,
                 birthday: formatDate
-            }
+            };
         });
 
         res.send(updatedData[0]);
+
     } catch (err) {
         res.status(500).send(err.message)
-    }
+    };
 };
 
 // essa função aqui é enviado por um post para crir a lista de cliente
 
 export async function customersPost(req, res) {
+
     // pegar os dados que a pessoa colocou na tela de cadastro de cliente
     const { name, phone, cpf, birthday } = req.body;
 
     try {
+
         // pegando toda a lista de clientes
         const listCustomers = await db.query(`
         SELECT * FROM customers;
-        `)
+        `);
+
         // verificar se o cpf ja foi cadastrado
         let cpfExists = false;
         listCustomers.rows.forEach(customer => {
             if (customer.cpf === cpf) {
                 cpfExists = true;
                 return;
-            }
+            };
         });
+
         if (cpfExists) {
             return res.sendStatus(409);
-        }
+        };
+
         // se tivertudo certo enviar para o Api
         const insertCustomers = await db.query(`
             INSERT INTO customers (name, phone, cpf, birthday) VAlUES ($1, $2, $3, $4);
@@ -85,34 +94,36 @@ export async function customersPost(req, res) {
         return res.sendStatus(201);
 
     } catch (err) {
-        res.status(500).send(err.message)
-
-    }
-}
+        res.status(500).send(err.message);
+    };
+};
 
 // essa função aqui é enviado para atualizar lista de cliente usando 0 id
 
 export async function customersPut(req, res) {
+
     // pegar os dados que a pessoa colocou na tela de cadastro de cliente
     const { name, phone, cpf, birthday } = req.body;
     const { id } = req.params;
 
     try {
+
         //verificar se o cpf ja existe
         const resultCustomers = await db.query(`SELECT * FROM customers WHERE cpf = $1;`, [cpf]);
+
         // cpf que quer ser alterado não é do dono
         if (resultCustomers.rows.length !== 0 && resultCustomers.rows[0].id !== parseInt(id)) {
             return res.sendStatus(409);
-        }
+        };
 
-       // se tivertudo certo enviar para o Api
+        // se tivertudo certo enviar para o Api
         const insertPutCustomers = await db.query(`
         UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5;
         ` , [name, phone, cpf, birthday, parseInt(id)]);
 
         return res.sendStatus(200);
-    } catch (err) {
-        res.status(500).send(err.message)
 
-    }
-}
+    } catch (err) {
+        res.status(500).send(err.message);
+    };
+};
